9fc42380812a11179ca48c94dd86a00e6546fa9c
KDH <eogh819@naver.com>
2020-09-28 22:08:14 +0900

    Add Test_002 ;StringFormat.cs - string format test script

---
+++ ProjectUnity/Assets/Test_002/StringFormat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StringFormat : MonoBehaviour
+{
+    public Text text;
+
+    public float aaa = 50;
+    public float bbb = 100;
+    public float ccc = 200;
+    public float ddd = 500;
+    public float fff = 1000;
+
+    /// <summary>
+    /// string format test
+    /// </summary>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = string.Format("aaa {0} , bbb {1} , ccc {2} , ddd {3}\n fff : {4} ({5}%)\n",
+            aaa, bbb, ccc, ddd, fff,
+            ((aaa / (bbb + ccc)) * 100f).ToString("F1"));
+    }
+
+ 
+}
